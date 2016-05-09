040641825b4a91e298c85edd16444c0190c39dc7
Yoshiwara Masashi <yoshiwara.masashi@tis.co.jp>
2016-05-09 16:38:53 +0900

    テストを追加しました。

---
+++ src/test/java/jp/co/tis/util/CsvReaderImplTest.java
@@ -0,0 +1,22 @@
+package jp.co.tis.util;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * CsvReaderImplをテストするクラス。
+ *
+ * @author Yoshiwara Masashi
+ * @since 1.0
+ */
+public class CsvReaderImplTest {
+
+    /**
+     * テスト。
+     */
+    @Test
+    public void test() {
+        fail("まだ実装されていません。");
+    }
+}
